b20ff3a15f59cdfd770c01893fecaccb6f422919
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-08-31 15:39:14 +0300

    Add basic server functionality

--- shopping-lists/app.js
+++ shopping-lists/app.js
@@ -1,8 +1,30 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { configure } from "https://deno.land/x/eta@v1.12.3/mod.ts";
+import * as listController from "./controllers/listController.js";
+
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
+
 
 const handleRequest = async (request) => {
-  console.log("Responding with Hello world!");
-  return new Response("Hello world!");
+  const url = new URL(request.url);
+
+  // Change after mainpage is ready
+  if (url.pathname === "/" && request.method === "GET") {
+    return new Response(`Redirecting to /lists.`, {
+      status: 303,
+      headers: {
+        "Location": "/lists",
+      },
+    });
+  } else if (url.pathname === "/lists" && request.method === "POST") {
+    return await listController.addNewList(request);
+  } else if (url.pathname === "/lists" && request.method === "GET") {
+    return await listController.viewLists(request);
+  } else {
+    return new Response("List not found", { status: 404});
+  }
 };
 
 serve(handleRequest, { port: 7777 });
