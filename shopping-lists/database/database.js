946b421b4a0ebf9b8bd28118abe1381a45691cbc
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-08-31 14:27:57 +0300

    Create DB connection pool

---
+++ shopping-lists/database/database.js
@@ -0,0 +1,38 @@
+import { Pool } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
+
+/*
+ * A connection pool is used to take care of connections.
+ * The number of concurrent connections is set to 3 by default.
+ */
+
+const CONCURRENT_CONNECTIONS = 3;
+const connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
+
+const executeQuery = async (query, params) => {
+    const response = {};
+    let client;
+
+    try {
+        client = await connectionPool.connect();
+        const result = await client.queryObject(query, params);
+
+        if (result.rows) {
+            response.rows = result.rows;
+        }
+    } catch (error) {
+        console.log(error);
+        response.error = e;
+    } finally {
+        if (client) {
+            try {
+                await client.release();
+            } catch (error) {
+                console.log("Error while releasing database connection.");
+                console.log(error);
+            }
+        }
+    }
+    return response;
+};
+
+export { executeQuery };
