f0e735110514f2eb71c69b60ab2a877a320856d2
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-08-31 14:48:42 +0300

    Add listing service for DB lists

---
+++ shopping-lists/services/shoppingListService.js
@@ -0,0 +1,22 @@
+import { executeQuery } from "../database/database";
+
+/*
+ * A service for adding new shopping lists and listing them.
+ * List is active by default based on the DB schema.
+ */
+
+const addNewList = async (name) => {
+    await executeQuery("INSERT INTO shopping_lists (name) VALUES ($name);", {
+        name: name,
+    });
+};
+
+const findAllActiveLists = async () => {
+    let result = await executeQuery(
+        "SELECT * FROM shopping_lists WHERE active = true;"
+    );
+
+    return result.rows;
+}
+
+export { addNewList, findAllActiveLists };
