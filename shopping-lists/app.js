715f8f41de37ba2679d3e88f18606ccf5491ae5d
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-04 14:21:02 +0300

    Add itemController to app.js

--- shopping-lists/app.js
+++ shopping-lists/app.js
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { configure } from "https://deno.land/x/eta@v1.12.3/mod.ts";
 import * as listController from "./controllers/listController.js";
+import * as itemController from "./controllers/itemController.js";
 
 configure({
   views: `${Deno.cwd()}/views/`,
@@ -24,6 +25,8 @@ const handleRequest = async (request) => {
     return await listController.viewLists(request);
   } else if (url.pathname.match("lists/[0-9]+") && request.method === "GET") {
     return await listController.viewItems(request);
+  } else if (url.pathname.match("lists/[0-9]+/items") && request.method === "POST") {
+    return await itemController.createItem(request);
   } else {
     return new Response("List not found", { status: 404});
   }
